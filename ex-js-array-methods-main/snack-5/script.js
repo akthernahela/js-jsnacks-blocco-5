0ed6143a62278f83b6d0e4f59ce6da54f8425a39
akthernahela <nahela00@gmail.com>
2025-10-22 15:44:16 +0200

    Find even number with forEach cycle

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -3,11 +3,23 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 
 let evenNumArray = [];
-for (let i = 0; i < nums.length; i++) {
+
+//Cycle for
+/*for (let i = 0; i < nums.length; i++) {
     const evenNum = nums[i];
     if(evenNum % 2 === 0){
         evenNumArray.push(evenNum)  
     }
-}
+}*/
+
+//Cycle forEach
+nums.forEach(evenNum => { 
+    if(evenNum % 2 === 0){
+        evenNumArray.push(evenNum)  
+    }
+});
+
+
+
 console.log(evenNumArray); 
 // Risultato: [2, 8, 4, 12]
