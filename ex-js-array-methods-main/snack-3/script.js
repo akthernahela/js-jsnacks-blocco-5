938687e4d64584bf19db096b7d953271d7f2ebb2
akthernahela <nahela00@gmail.com>
2025-10-22 15:03:59 +0200

    Add the forEach cycle

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,10 +2,20 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 const increasedNum = [];
-for(let i = 0; i < numbers.length; i++){
+
+//Cycle for
+/*for(let i = 0; i < numbers.length; i++){
     let num = numbers[i];
     num++
     increasedNum.push(num)
-}
+};*/
+
+//Cycle forEach
+numbers.forEach(number => {
+    number++
+    increasedNum.push(number)
+});
+
+
 // Risultato: [3, 9, 5, 8, 3, 88]
 console.log(increasedNum);
