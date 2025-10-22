cfce8a121619949cef89816d57f432878f9b66ae
akthernahela <nahela00@gmail.com>
2025-10-22 15:37:32 +0200

    Add map metod to the third snack

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,7 +1,7 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
-const increasedNum = [];
+//const increasedNum = [];
 
 //Cycle for
 /*for(let i = 0; i < numbers.length; i++){
@@ -11,11 +11,15 @@ const increasedNum = [];
 };*/
 
 //Cycle forEach
-numbers.forEach(number => {
+/*numbers.forEach(number => {
     number++
     increasedNum.push(number)
-});
+});*/
+
+//map()
+const increasedNum = numbers.map( number => number + 1);
+console.log(increasedNum);
 
 
 // Risultato: [3, 9, 5, 8, 3, 88]
-console.log(increasedNum);
+//console.log(increasedNum);
