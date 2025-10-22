76488f8ed33cf79d38fd8a23bd55756e57caff87
akthernahela <nahela00@gmail.com>
2025-10-22 15:01:08 +0200

    Increase number with cycle for

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+const increasedNum = [];
+for(let i = 0; i < numbers.length; i++){
+    let num = numbers[i];
+    num++
+    increasedNum.push(num)
+}
 // Risultato: [3, 9, 5, 8, 3, 88]
+console.log(increasedNum);
