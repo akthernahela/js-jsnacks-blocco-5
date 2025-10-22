02eab38c59b990d2186980017e3929f8e6acd28b
akthernahela <nahela00@gmail.com>
2025-10-22 14:21:40 +0200

    First snack

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,6 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+const nameString = names.join(', ');
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+console.log(nameString);
