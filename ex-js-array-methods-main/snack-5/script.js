51b0b60d0094389e343d48205eb9236c95b580bd
akthernahela <nahela00@gmail.com>
2025-10-22 15:28:20 +0200

    Search the even number still not finish

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,11 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+for (let i = 0; i < nums.length; i++) {
+    const evenNum = [i];
+    if(evenNum % 2 === 0){
+        console.log();
+        
+    }
+}
 // Risultato: [2, 8, 4, 12]
